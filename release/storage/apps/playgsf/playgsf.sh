db10cdc362be465c1242cf2ae05c2e32a960184d
Ninoh-FOX <ninohfox@gmail.com>
2026-02-04 03:41:13 +0100

    added build files

---
+++ release/storage/apps/playgsf/playgsf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2020-present Fewtarius
+
+source /usr/bin/env.sh
+source /etc/profile
+
+jslisten set "killall playgsf"
+
+/storage/apps/playgsf/selector_playgsf
+sync
